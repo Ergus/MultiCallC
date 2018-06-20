7ffcc0a07106ddd5a9e93a40f43a24bcd663ef6d
Jimmy Aguilar Mena <kratsbinovish@gmail.com>
2018-06-20 16:44:56 +0200

    Improve in the C-java interface.
    This is just a small correction to make the test more similar to the
    other examples.
    I also changed some filenames to make a common pattern for all the examples.

--- main_j.java
+++ main_j.java
@@ -4,36 +4,37 @@
 // In Java an externa wrapper is mandatory
 
 public class main_j {
-  native int square(int i);
-  native int cube(int i);
-  native int print(int i, double [] array);
-  native int printstr(String str);
-        
-  static {
-    System.loadLibrary("funjava");
-    }
-  static public void main(String argv[]) {
-      
-    // First Part      
-    main_j libfun = new main_j() ;
-    
-    for(int i=0;i<5;i++){
-      System.out.printf("%d\tx^2=%2d\tx^3=%2d\n",
-                        i,libfun.square(i),libfun.cube(i));
-      }
+	native int square(int i);
+	native int cube(int i);
+	native void print(int i, double [] array);
+	native void printstr(String str);
 
-    // Second part
-    int size=6;
-    double[] array = new double[size];
-    
-    for(int i=0;i<size;i++) array[i]=i+1;
-    
-    libfun.print(size,array);
-
-    // Third part
-    String str="my string";
-    libfun.printstr(str);
-    
+	static {
+		System.loadLibrary("modulejava");
     }
-        
-  }
+
+	static public void main(String argv[])
+	{
+		// First Part
+		main_j libfun = new main_j() ;
+
+		for(int i = 0; i < 5; ++i) {
+			System.out.printf("%d\tx^2=%2d\tx^3=%2d\n",
+							  i,libfun.square(i),libfun.cube(i));
+		}
+
+		// Second part
+		int size = 6;
+		double[] array = new double[size];
+
+		for(int i = 0; i < size; ++i)
+			array[i] = i + 1;
+
+		libfun.print(size, array);
+
+		// Third part
+		String str="my string";
+		libfun.printstr(str);
+    }
+
+}
---
+++ src/c-java.cc
@@ -0,0 +1,36 @@
+// Wraper for Java execution generated using
+// the header from javah.
+
+#include <jni.h>
+#include <stdio.h>
+
+#include "c-java.h"
+#include "libfun.h"
+
+JNIEXPORT jint JNICALL Java_main_1j_square(JNIEnv *, jobject, jint x)
+{
+	int tmp = x;
+	return square(tmp);
+}
+
+JNIEXPORT jint JNICALL Java_main_1j_cube(JNIEnv *, jobject, jint x)
+{
+	int tmp = x;
+	return cube(tmp);
+}
+
+JNIEXPORT void JNICALL Java_main_1j_print
+(JNIEnv *env, jobject, jint size, jdoubleArray array)
+{
+	jdouble *tmp_array = env->GetDoubleArrayElements(array, 0);
+
+	print(size,tmp_array);
+
+	env->ReleaseDoubleArrayElements(array, tmp_array, 0);
+}
+
+JNIEXPORT void JNICALL Java_main_1j_printstr (JNIEnv *env, jobject, jstring str)
+{
+	const char *tmpstr = env->GetStringUTFChars(str, 0);
+	printstr(tmpstr);
+}
