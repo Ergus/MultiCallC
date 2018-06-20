64501104b453d61c14cc07d61bdf6d709b311361
Jimmy Aguilar Mena <kratsbinovish@gmail.com>
2018-06-20 16:51:39 +0200

    Small correction in the guile example to correct indices

--- main.scm
+++ main.scm
@@ -11,13 +11,13 @@
   (format #t "~s\tx^2=~s\tx^3=~s\n" i (square i) (cube i)))
 
 ;; Array
-(define size 5)
+(define size 6)
 
 (define array (make-f64vector size))
 
 (do ((i 0 (1+ i)))
     ((>= i size))
-  (f64vector-set! array i i))
+  (f64vector-set! array i (+ i 1)))
 (print size array)
 
 ;; string
