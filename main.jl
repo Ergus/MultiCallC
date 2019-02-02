0adbeff2a4b549b749b05248da64577807cbd800
Jimmy Aguilar Mena <kratsbinovish@gmail.com>
2019-02-02 04:32:30 +0100

    Updated Julia test, there were some changes in the Julia side

--- main.jl
+++ main.jl
@@ -17,20 +17,21 @@
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 #
 
+import Printf
 # First part
 for i = 0:4
     square = ccall( (:square,"./libfun"), Cint, (Cint,), i )
     cube   = ccall( (:cube,  "./libfun"), Int64, (Int64,), i ) #same than above
-    @printf("%d\tx^2=%2d\tx^3=%2d\n",i,square,cube)
+    Printf.@printf("%d\tx^2=%2d\tx^3=%2d\n",i,square,cube)
 end
 
 # Second part
 size=6
 arr=collect(1.: 6)
-ccall( (:print, "./libfun"), Void, (Cint, Ptr{Cdouble}), size, arr )
+ccall( (:print, "./libfun"), Cvoid, (Cint, Ptr{Cdouble}), size, arr )
 
 # Third part
 mystring="my string"
 
-ccall( (:printstr, "./libfun"), Void, (Cstring,), mystring)
+ccall( (:printstr, "./libfun"), Cvoid, (Cstring,), mystring)
 
